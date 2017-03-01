a78bf4d301263dccc67b7fbdd53ebe66b10ffd95
Jessica Prado <jessica.marie.prado@gmail.com>
2017-02-28 19:46:48 -0500

    progress

--- controllers/news_controller.js
+++ controllers/news_controller.js
@@ -1,38 +1,41 @@
+//required modules
 var express = require('express');
 var app = express();
 var request = require('request');
 var cheerio = require('cheerio');
 var mongoose = require('mongoose');
+//required js files
 var db = require('./../config/connection.js');
-
 var Articles = require('./../models/news.js');
 var Comments = require('./../models/comments.js');
 
 app.get('/', function(req, res) {
 	Articles.find(function(err, articles, doc) {
 		res.render('index', {Articles : articles});
-	}).limit(5);
+	}).limit(10);
 })
 
-//Start of cheerio
+// scrapes data from website when "MORE PIG NEWS" button is selected
 app.get('/scrape', function(req, res) {
-
- 	var url = 'https://www.reddit.com/r/UpliftingNews/';
+	//Start of cheerio
+ 	var url = 'http://www.goodnewsnetwork.org/uplift/inspiring/';
 
  	request(url, function(err, res, body) {
 
  		var $ = cheerio.load(body);
 		
- 		$('.thing').each(function(i, element){
+ 		$('.td_mod2').each(function(i, element){
  			
  		 	var result = {};
 
- 		 	result.title = $(this).children(".entry").children("p.title").children("a.title").text();
+ 		 	result.title = $(this).children("h3.entry-title").children("a").text();
 
- 		 	result.link = $(this).children(".entry").children("p.title").children("a.title").attr('href');
+ 		 	result.link = $(this).children("h3.entry-title").children("a").attr('href');
 			
-			result.image = $(this).children().children('img').attr('src');
+			result.image = $(this).children(".thumb-wrap").children('a').children("img").attr('src');
  		 	
+ 		 	console.log("Title: " + result.image);
+
  		 	var entry = new Articles(result);
 			
  		 	entry.save(function(err, doc){
@@ -46,5 +49,18 @@ app.get('/scrape', function(req, res) {
  	})
  	res.redirect('/');
  })
+
+//return comments based on the article clicked
+app.get('/viewComments', function(req, res){
+
+	Comments.find({}, function(error, found){
+
+		if(error) {
+			console.log(error)
+		} else {
+			res.json(found);
+		}
+	})
+});
 
 module.exports = app;
